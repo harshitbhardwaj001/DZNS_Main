2ae4ae540c2f6d4b49bec7a805de2c39fcd87dc6
Harshit <harshitbhardwaj001@gmail.com>
2024-01-02 03:24:34 +0530

    Favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       {/* <meta
         httpEquiv="Content-Security-Policy"
         content="script-src 'self' https://js.stripe.com/v3 'unsafe-inline' 'unsafe-eval'"
