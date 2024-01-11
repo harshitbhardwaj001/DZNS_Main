63f72b7253e87209475c462401072e72ac441cc7
Harshit <harshitbhardwaj001@gmail.com>
2024-01-12 02:34:21 +0530

    Finally

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const withTM = require("next-transpile-modules")(["gsap"]);
 
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = withTM({
   env: {
-    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    SERVER_URL: isProduction
+      ? "https://your-production-server-url"
+      : "http://localhost:9001",
   },
   images: {
-    domains: ["**"],
+    domains: ["localhost"],
   },
 });
