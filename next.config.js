29a5a88c205f9afb5bae84b6f3af770e235b39c6
Harshit <harshitbhardwaj001@gmail.com>
2024-01-11 22:28:11 +0530

    I am tired

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withTM = require("next-transpile-modules")(["gsap"]);
 
 module.exports = withTM({
+  env: {
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  },
   images: {
     domains: ["**"],
   },
