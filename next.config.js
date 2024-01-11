2e499f57177974c42f3bcec3712b1b3901c09a34
Harshit <harshitbhardwaj001@gmail.com>
2024-01-11 21:31:55 +0530

    Deploy Test5

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 const withTM = require("next-transpile-modules")(["gsap"]);
 
-const isProduction = process.env.NODE_ENV === "production";
-
 module.exports = withTM({
-  // env: {
-  //   SERVER_URL: isProduction
-  //     ? "https://your-production-server-url"
-  //     : "http://localhost:9001",
-  // },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: ["**"],
-        port: "",
-        pathname: "**",
-      },
-    ],
+    domains: ["localhost"],
   },
 });
