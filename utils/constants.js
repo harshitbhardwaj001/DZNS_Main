a2f702b98789010f753ce0f277c322a0addd9b18
Harshit <harshitbhardwaj001@gmail.com>
2024-01-12 00:23:47 +0530

    test

--- utils/constants.js
+++ utils/constants.js
@@ -13,19 +13,19 @@ export const GET_USER_INFO = `${AUTH_ROUTES}/get-user-info`;
 export const SET_USER_INFO = `${AUTH_ROUTES}/set-user-info`;
 export const SET_USER_IMAGE = `${AUTH_ROUTES}/set-user-image`;
 
-export const ADD_SERVICE_ROUTE = `${SERVICES_ROUTES}/add`;
-export const GET_USER_SERVICES_ROUTE = `${SERVICES_ROUTES}/get-user-services`;
-export const GET_SERVICE_DATA = `${SERVICES_ROUTES}/get-service-data`;
-export const EDIT_SERVICE_ROUTE = `${SERVICES_ROUTES}/edit-service`;
-export const SEARCH_SERVICE_ROUTE = `${SERVICES_ROUTES}/search-services`;
-export const CHECK_USER_ORDERED_SERVICE_ROUTE = `${SERVICES_ROUTES}/check-service-order`;
+// export const ADD_SERVICE_ROUTE = `${SERVICES_ROUTES}/add`;
+// export const GET_USER_SERVICES_ROUTE = `${SERVICES_ROUTES}/get-user-services`;
+// export const GET_SERVICE_DATA = `${SERVICES_ROUTES}/get-service-data`;
+// export const EDIT_SERVICE_ROUTE = `${SERVICES_ROUTES}/edit-service`;
+// export const SEARCH_SERVICE_ROUTE = `${SERVICES_ROUTES}/search-services`;
+// export const CHECK_USER_ORDERED_SERVICE_ROUTE = `${SERVICES_ROUTES}/check-service-order`;
 
-export const CREATE_ORDER = `${ORDERS_ROUTES}/create`;
-export const ORDER_SUCCESS_ROUTE = `${ORDERS_ROUTES}/success`;
-export const GET_SELLER_ORDERS_ROUTE = `${ORDERS_ROUTES}/get-seller-orders`;
-export const GET_BUYER_ORDERS_ROUTE = `${ORDERS_ROUTES}/get-buyer-orders`;
+// export const CREATE_ORDER = `${ORDERS_ROUTES}/create`;
+// export const ORDER_SUCCESS_ROUTE = `${ORDERS_ROUTES}/success`;
+// export const GET_SELLER_ORDERS_ROUTE = `${ORDERS_ROUTES}/get-seller-orders`;
+// export const GET_BUYER_ORDERS_ROUTE = `${ORDERS_ROUTES}/get-buyer-orders`;
 
-export const GET_MESSAGES = `${MESSAGES_ROUTES}/get-messages`;
-export const ADD_MESSAGES = `${MESSAGES_ROUTES}/add-message`;
+// export const GET_MESSAGES = `${MESSAGES_ROUTES}/get-messages`;
+// export const ADD_MESSAGES = `${MESSAGES_ROUTES}/add-message`;
 
-export const GET_SELLER_DASHBOARD_DATA = `${DASHBOARD_DATA_ROUTES}/seller`;
+// export const GET_SELLER_DASHBOARD_DATA = `${DASHBOARD_DATA_ROUTES}/seller`;
