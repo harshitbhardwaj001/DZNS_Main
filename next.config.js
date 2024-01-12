038893b5d32c99ec2e2dfc906b4b52b67b10a051
Harshit <harshitbhardwaj001@gmail.com>
2024-01-12 20:40:00 +0530

    I think this will work

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 const withTM = require("next-transpile-modules")(["gsap"]);
 
-const isProduction = process.env.NODE_ENV === "production";
-
 module.exports = withTM({
-  env: {
-    SERVER_URL: isProduction
-      ? "https://your-production-server-url"
-      : "http://localhost:9001",
-  },
   images: {
-    domains: ["localhost"],
+    // Use an array of objects with hostname property
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**"
+      },
+    ],
   },
 });
